f51d320c6e8c761f8ec1e33bc645d4483d08375f
Navid <nahardaninavid1993@gmail.com>
2023-11-07 20:02:31 +0330

    fetching countries

---
+++ components/CountryCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-slate-100 p-8 rounded-2xl hover:shadow-lg hover:cursor-pointer"
+  >
+    <div>
+      <img class="block w-full h-auto" :src="country.flags.png" />
+    </div>
+    <div class="py-4 text-xl font-bold">
+      {{ country.name.common }}
+    </div>
+    <Button
+      @click="addToFav()"
+      class="px-3 py-2 rounded-md"
+      :class="addedToFav ? 'bg-red-500' : 'bg-red-300'"
+      >add to fav!</Button
+    >
+  </div>
+</template>
+<script setup>
+import { object } from "vue-types";
+const props = defineProps({
+  country: object(),
+});
+const addedToFav = ref(false);
+const addToFav = () => {
+  addedToFav.value = !addedToFav.value;
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <main>
+  <main class="bg-slate-50">
     <div class="container mx-auto">
-      <ul class="bg-red-400">
-        <li>hello</li>
+      <ul class="flex flex-wrap pt-11">
+        <li
+          class="w-1/4 basis-1/4"
+          v-for="country in countryList"
+          :key="country.cca3"
+        >
+          <div class="p-2">
+            <CountryCard :country="country" />
+          </div>
+        </li>
       </ul>
     </div>
   </main>
@@ -18,15 +26,14 @@
 //   }
 
 //   getCountries()
-import { useFavCountry } from '@/stores/favCountries.js'
-const countryList = ref([])
-const store = useFavCountry()
+// import { useFavCountry } from "@/stores/favCountries.js";
+// const store = useFavCountry();
+const countryList = ref([]);
 const fetchCountries = async () => {
-  countryList.value = []
-  const { data } = await useFetch('https://restcountries.com/v3.1/all')
-  countryList.value = data.value || []
-  store.countriesList = data.value || []
-}
-fetchCountries()
-console.log(store.favCountryList)
+  countryList.value = [];
+  const { data } = await useFetch("https://restcountries.com/v3.1/all");
+  countryList.value = data.value || [];
+  // store.countriesList = data.value || [];
+};
+fetchCountries();
 </script>
