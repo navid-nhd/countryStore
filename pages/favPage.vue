2c56c8fe56ca4295fab08c879a325a977188ab31
Navid <nahardaninavid1993@gmail.com>
2023-11-08 09:47:33 +0330

    adding dynamic country borders

--- pages/favPage.vue
+++ pages/favPage.vue
@@ -1,32 +1,62 @@
 <template>
-    <div class="container mx-auto">
-
-        <ul class="flex flex-wrap pt-11">
-            <li class="w-1/4 basis-1/4" v-for="country in favoriteDetailedList" :key="country.cca3">
-                <div class="p-2">
-                    <CountryCard :country="country" />
-                </div>
-            </li>
-        </ul>
-        <div class="pt-32 pb-16">
-            <h2 class="font-bold text-red-400 text-2xl ">Boreders</h2>
+  <div class="container mx-auto">
+    <ul class="flex flex-wrap pt-11">
+      <li
+        class="w-1/4 basis-1/4"
+        v-for="country in favoriteDetailedList"
+        :key="country.cca3"
+      >
+        <div class="p-2">
+          <CountryCard :country="country" />
         </div>
+      </li>
+    </ul>
+    <div class="pt-16 pb-2">
+      <h2 class="font-bold text-red-400 text-2xl">Boreders</h2>
     </div>
+    <ul class="flex flex-wrap">
+      <li
+        class="w-1/4 basis-1/4"
+        v-for="country in bordersDetail"
+        :key="country.cca3"
+      >
+        <div class="p-2">
+          <CountryCard :country="country" />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script setup>
-import { includes } from 'lodash'
-import { useFavCountry } from '@/stores/favCountries.js'
-const store = useFavCountry()
+import { includes, uniqBy } from "lodash";
+import { useFavCountry } from "@/stores/favCountries.js";
+const store = useFavCountry();
 const favoriteDetailedList = computed(() => {
-    try {
-        return store.favCountryList
-    } catch (error) {
-        return []
-    }
-})
-watch(favoriteDetailedList.value, () => {
-    console.log('hello')
-}, { immediate: true })
+  try {
+    return store.favCountryList;
+  } catch (error) {
+    return [];
+  }
+});
+const bordersList = ref([]);
+watch(
+  favoriteDetailedList.value,
+  () => {
+    bordersList.value = [];
+    // console.log(favoriteDetailedList.value);
+    bordersList.value = favoriteDetailedList.value.map((item) =>
+      item?.borders ? item?.borders : ""
+    );
+  },
+  { immediate: true }
+);
+const uniquedBorders = computed(() => {
+  try {
+    return uniqBy(bordersList.value.flat().filter((item) => item));
+  } catch (error) {
+    return [];
+  }
+});
 // const bordersString = ref("");
 // const paramName = route.params.name.replaceAll("-", " ");
 
@@ -37,7 +67,19 @@ watch(favoriteDetailedList.value, () => {
 // coutryDetail.value[0].borders.forEach((item) => {
 //   bordersString.value = bordersString.value + item + ",";
 // });
-// const { data: bordersDetail } = await useFetch(
-//   `https://restcountries.com/v3.1/alpha?codes=${bordersString.value}`
-// );
+const bordersDetail = ref([]);
+watch(
+  () => uniquedBorders.value,
+  async () => {
+    bordersDetail.value = [];
+    const { data } = await useFetch(
+      `https://restcountries.com/v3.1/alpha?codes=${uniquedBorders.value
+        .flat()
+        .join()}`
+    );
+    console.log(data.value);
+    bordersDetail.value = data.value || [];
+  },
+  { immediate: true }
+);
 </script>
--- stores/favCountries.js
+++ stores/favCountries.js
@@ -1,30 +1,30 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useFavCountry = defineStore('favCountry', {
+export const useFavCountry = defineStore("favCountry", {
   state: () => {
     return {
       countriesList: [],
-      favCountryList: []
-    }
+      favCountryList: [],
+      borderList: [],
+    };
   },
   actions: {
     updateList(countryCca3) {
       const countryExistInList = this.favCountryList.some(
-        item => item.cca3 == countryCca3
-      )
+        (item) => item.cca3 == countryCca3
+      );
       if (!countryExistInList) {
         const indexOfCountry = this.countriesList.findIndex(
-          item => item.cca3 == countryCca3
-        )
-        // console.log(indexOfCountry)
-        this.favCountryList.push(this.countriesList[indexOfCountry])
+          (item) => item.cca3 == countryCca3
+        );
+        this.favCountryList.push(this.countriesList[indexOfCountry]);
       } else if (countryExistInList) {
-        const index = this.favCountryList.findIndex(item => item.cca3 === countryCca3)
-        this.favCountryList.splice(index, 1)
+        const index = this.favCountryList.findIndex(
+          (item) => item.cca3 === countryCca3
+        );
+        this.favCountryList.splice(index, 1);
       }
-    }
+    },
   },
-  getters: {
-
-  }
-})
+  getters: {},
+});
